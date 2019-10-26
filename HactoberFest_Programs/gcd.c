b0a8cf3d7d4ab139b255dc0c2fa9aa6a6269b676
shravanbr <57026750+shravanbr@users.noreply.github.com>
2019-10-26 10:48:10 +0530

    code inside gcd function
    inserted if-else condition inside gcd function.

--- HactoberFest_Programs/gcd.c
+++ HactoberFest_Programs/gcd.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
-int gcd(int a,int b){
-	//Write code here
+int gcd(int num1,int num2){
+     if(num2!=0)
+	     return gcd(num2,num1%num2);
+	else
+		return num1;
 }
 int main(){
 	int num1,num2,res=0;
